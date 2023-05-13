317c0930b649cf3c8dbdc45beeb0244e1735635c
Raja Dishafnya Achadi <uno.highclass2@gmail.com>
2023-05-14 03:37:02 +0800

    Add Full Image Positioning Functionality

--- ViewModels/Onimai2TextViewModel.cs
+++ ViewModels/Onimai2TextViewModel.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using System.Drawing;
 using System.IO;
+using System.Collections.ObjectModel;
 using Prism.Commands;
 using Prism.Mvvm;
 using OpenCvSharp;
@@ -21,14 +22,17 @@ namespace SD2CircleTool.ViewModels
 
         private Mat image;
         private double imageRatio { get { return (double)image.Cols / (double)image.Rows; } }
-        private AlignmentInfo align;
-
+        private bool keepRatio; public bool KeepRatio { get { return keepRatio; } set { SetProperty(ref keepRatio, value); } }
+        private AlignmentInfo alignment; public AlignmentInfo Alignment { get { return alignment; } set { SetProperty(ref alignment, value); UpdateRectPosition(); } }
+        private ObservableCollection<AlignmentInfo> alignments; public ObservableCollection<AlignmentInfo> Alignments { get { return alignments; } set { SetProperty(ref alignments, value); } }
 
         #region Real Image Values
-        private double x; public double X { get { return x; } set { SetProperty(ref x, value); dX = value * s2unitRatio; } }
-        private double y; public double Y { get { return y; } set { SetProperty(ref y, value); dY = value * s2unitRatio; } }
-        private double w; public double W { get { return w; } set { SetProperty(ref w, value); h = (int)((double)value / resRatio); dW = value * s2unitRatio; dH = h * s2unitRatio; } }
-        private double h; public double H { get { return h; } set { SetProperty(ref h, value); w = (int)((double)value * resRatio); dH = value * s2unitRatio; dW = w * s2unitRatio; } }
+        private double x; public double X { get { return x; } set { SetProperty(ref x, value); UpdateRectPosition(); } }
+        private double y; public double Y { get { return y; } set { SetProperty(ref y, value); UpdateRectPosition(); } }
+        private double angle; public double Angle { get { return angle; } set { SetProperty(ref angle, value); UpdateRectPosition(); } }
+        private double w; public double W { get { return w; } set { SetProperty(ref w, value); if (u) { u = false; H = (double)value / resRatio; u = true; } UpdateRectPosition(); } }
+        private double h; public double H { get { return h; } set { SetProperty(ref h, value); if (u) { u = false; W = (double)value * resRatio; u = true; } UpdateRectPosition(); } }
+        private bool u = true;
 
         #endregion
 
@@ -39,10 +43,12 @@ namespace SD2CircleTool.ViewModels
         private double dh; public double dH { get { return dh; } set { SetProperty(ref dh, value); } }
 
         #endregion
-        private int xRes; public int XRes { get { return xRes; } set { SetProperty(ref xRes, Math.Clamp(value, 1, 9999)); if (uRes) { uRes = false; YRes = (int)((double)value / imageRatio); UpdateFGImage(); uRes = true; } } }
-        private int yRes; public int YRes { get { return yRes; } set { SetProperty(ref yRes, Math.Clamp(value, 1, 9999)); if (uRes) { uRes = false; XRes = (int)((double)value * imageRatio); UpdateFGImage(); uRes = true; } } }
+        private int xRes; public int XRes { get { return xRes; } set { SetProperty(ref xRes, Math.Clamp(value, 1, 9999)); if (uRes) { uRes = false; if (KeepRatio) { YRes = (int)((double)value / imageRatio); } UpdateFGImage(); uRes = true; } } }
+        private int yRes; public int YRes { get { return yRes; } set { SetProperty(ref yRes, Math.Clamp(value, 1, 9999)); if (uRes) { uRes = false; if (KeepRatio) { XRes = (int)((double)value * imageRatio); } UpdateFGImage(); uRes = true; } } }
         private bool uRes = true;
         private double resRatio { get { return (double)xRes / (double)yRes; } }
+
+
 
         private BitmapSource bgImage;  public BitmapSource BGImage { get { return bgImage; } }
         private BitmapSource fgImage;  public BitmapSource FGImage { get { return fgImage; } set { SetProperty(ref fgImage, value); } }
@@ -68,6 +74,59 @@ namespace SD2CircleTool.ViewModels
             W = tempw;
         }
 
+        // Turn real coords to display coords
+        private void UpdateRectPosition()
+        {
+            dW = W * s2unitRatio; 
+            dH = h * s2unitRatio;
+
+            double tx = 224;
+            double ty = 126;
+
+            switch (Alignment)
+            {
+                case AlignmentInfo.Left:
+                    tx += X;
+                    tx += H * 0.5d * Math.Sin(Angle * Math.PI / 180d);
+
+                    dX = tx * s2unitRatio;
+
+                    ty -= Y;
+                    ty -= H * 0.5d * Math.Cos(Angle * Math.PI / 180d);
+
+                    dY = ty * s2unitRatio;
+                    break;
+                case AlignmentInfo.Right:
+                    tx += X;
+                    tx -= W * Math.Cos(Angle * Math.PI / 180d);
+                    tx += H * 0.5d * Math.Sin(Angle * Math.PI / 180d);
+
+                    dX = tx * s2unitRatio;
+
+                    ty -= Y;
+                    ty -= H * 0.5d * Math.Cos(Angle * Math.PI / 180d);
+                    ty -= W * Math.Sin(Angle * Math.PI / 180d);
+
+                    dY = ty * s2unitRatio;
+                    break;
+                case AlignmentInfo.Center:
+                default:
+                    tx += X;
+                    tx -= W * 0.5d * Math.Cos(Angle * Math.PI / 180d);
+                    tx += H * 0.5d * Math.Sin(Angle * Math.PI / 180d);
+
+                    dX = tx * s2unitRatio;
+
+                    ty -= Y;
+                    ty -= H * 0.5d * Math.Cos(Angle * Math.PI / 180d);
+                    ty -= W * 0.5d * Math.Sin(Angle * Math.PI / 180d);
+
+                    dY = ty * s2unitRatio;
+                    break;
+
+            }
+        }
+
         public DelegateCommand OpenFileCommand => new(() =>
         {
             _isPathValid = false;
@@ -89,11 +148,24 @@ namespace SD2CircleTool.ViewModels
             Mat bg = new Mat(@".\Assets\catMahiro.png");
             bgImage = BitmapSourceConverter.ToBitmapSource(bg);
 
-            align = AlignmentInfo.Center;
+            KeepRatio = true;
+
+            X = 0;
+            Y = 0;
+            Angle = 0;
+
+            Alignments = new ObservableCollection<AlignmentInfo>()
+            {
+                AlignmentInfo.Left,
+                AlignmentInfo.Center,
+                AlignmentInfo.Right
+            };
+
+            Alignment = AlignmentInfo.Center;
         }
     }
 
-    enum AlignmentInfo
+    public enum AlignmentInfo
     {
         Left, Center, Right
     }
